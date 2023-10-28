15b1a42beb76f46e9abbe60d2d207b695c04e134
alsun2001 <36275945+alsun2001@users.noreply.github.com>
2023-10-28 11:56:19 -0700

    Close automatically after reaching SWIPE_CLOSE_OFFSET

--- src/components/ImageItem/ImageItem.ios.tsx
+++ src/components/ImageItem/ImageItem.ios.tsx
@@ -28,7 +28,7 @@ import { ImageSource } from "../../@types";
 import { ImageLoading } from "./ImageLoading";
 
 const SWIPE_CLOSE_OFFSET = 75;
-const SWIPE_CLOSE_VELOCITY = 1.55;
+const SWIPE_CLOSE_VELOCITY = 1;
 const SCREEN = Dimensions.get("screen");
 const SCREEN_WIDTH = SCREEN.width;
 const SCREEN_HEIGHT = SCREEN.height;
@@ -103,6 +103,10 @@ const ImageItem = ({
 
     if (nativeEvent?.zoomScale > 1) {
       return;
+    }
+
+    if (Math.abs(offsetY) > SWIPE_CLOSE_OFFSET) {
+      onRequestClose();
     }
 
     scrollValueY.setValue(offsetY);
